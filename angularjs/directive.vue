5b0728aeafa71abf93db5c93e4117049e1c42e6d
Youssef Gamil <yoga1290@gmail.com>
2018-03-11 23:54:25 +0200

    electron + angularjs

---
+++ angularjs/directive.vue
@@ -0,0 +1,25 @@
+<template lang="jade">
+pre
+  div
+    |angular.module('app').directive('{{Capitalise(object.name}}', function () {
+    |   return {
+    |       restrict: 'E',
+    |       templateUrl: '{{camelCaseTo_(object.name)}}.html',
+    |       scope: {
+  div(v-for="field in object.fields")
+    |           "{{field.name}}": '={{field.name}}',
+  div
+    |       },
+    |       controller: '{{Capitalise(object.name)}}Ctrl'
+    |       link: {
+    |         post: function(scope, element, attrs) {}
+    |       }
+    |     };
+    |   });
+</template>
+
+<script>
+import Template from 'Template'
+import {fieldDef, objectDef} from './config.json'
+export default Template(fieldDef, objectDef)
+</script>
